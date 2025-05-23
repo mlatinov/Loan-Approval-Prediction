4bb4fa1c1ca83d0157c922eb9fe1c88f7b8e74f4
mlatinov <latinovmetodi@gmail.com>
2025-05-23 20:49:49 +0300

    # get the data and clean it for eda

---
+++ functions/data_clean_fuction.R
@@ -0,0 +1,16 @@
+
+clean_eda <- function(data){
+  
+  data <- data %>%
+    # Change data types
+    mutate(
+      loan_intent = str_to_lower(loan_intent),
+      person_home_ownership = str_to_lower(person_home_ownership),
+      loan_status = factor(loan_status)
+      )%>%
+    
+    # Convert all char into factors
+    mutate(across(where(is.character), as.factor))
+  
+  return(data)
+}
