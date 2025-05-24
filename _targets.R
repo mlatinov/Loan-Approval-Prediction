3fb95397e12461c27bcc43cc41eb811024a4c16f
mlatinov <latinovmetodi@gmail.com>
2025-05-24 16:33:40 +0300

    # Split the data

--- _targets.R
+++ _targets.R
@@ -2,11 +2,13 @@
 #### Libraries ####
 library(targets)
 library(tarchetypes)
+library(tidymodels)
+
 report_dir <- normalizePath("Report_documents")
 
 # Set targets options
 tar_option_set(
-  packages = c("tidyverse","patchwork","ggcorrplot","DT"),
+  packages = c("tidyverse","patchwork","ggcorrplot","DT","tidymodels"),
   seed = 123)
 
 # Source functions
@@ -16,13 +18,13 @@ tar_source("functions/eda_functions.R")
 # Workflows
 list(
   
-    ## Load the data ##
+    #### Load the data ####
     tar_target(name = file_train,command = "Data/train.csv",format = "file"),
     tar_target(name = file_test,command = "Data/test.csv",format = "file"),
     tar_target(name = loan_train_data,command = read_csv(file = file_train)),
     tar_target(name = loan_test_data,command = read_csv(file = file_test)),
 
-    ## EDA ##
+    #### EDA ####
     # Prepare data for EDA
     tar_target(name = loan_train_eda,command = clean_eda(data = loan_train_data)),
     
@@ -33,7 +35,33 @@ list(
       output_file = file.path(report_dir, "Eda_report.html"),
       params = list(eda_data = loan_train_eda),
       quiet = FALSE
-    )
+    ),
+    
+    #### Modeling ####
+    
+    ## Split the data
+    tar_target(
+      name = data_split,
+      command = initial_validation_split(
+        data = loan_train_eda,
+        strata = "loan_status")),
+    
+    ## Get the data 
+    
+    # Model train data 
+    tar_target(
+      name = training_data,
+      command = training(data_split)),
+    
+    # Model test data
+    tar_target(
+      name = testing_data,
+      command = testing(data_split)),
+    
+    # Validation data
+    tar_target(
+      name = validation_data,
+      command = validation(data_split))
 )
 
 
