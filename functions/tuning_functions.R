79941ed2d54d202d4fc78afa22e0e577acd23166
mlatinov <latinovmetodi@gmail.com>
2025-05-26 22:04:46 +0300

    # tune_race_win_lose

--- functions/tuning_functions.R
+++ functions/tuning_functions.R
@@ -4,8 +4,7 @@
 #### Light Tuning ####
 
 ## Tune Race Anova Function 
-
-tune_race_aov <- function(workflow, grid = 10, burn_in = 3,resamples) {
+tune_race_aov <- function(workflow, burn_in = 3,resamples,size = 20,params) {
   
   # Define control for ANOVA race
   control <- control_race(
@@ -16,30 +15,112 @@ tune_race_aov <- function(workflow, grid = 10, burn_in = 3,resamples) {
   )
   
   # Set metric set
-  metric <- metric_set(roc_auc)
+  metric_aov <- metric_set(roc_auc)
+  
+  # Custom Grid based on the params
+  custom_grid <- grid_random(params, size = size)
   
   # Perform tuning
   race_results <- tune_race_anova(
     object = workflow,
     resamples = resamples,
-    grid = grid,
-    metrics = metric,
+    grid = custom_grid,
+    metrics = metric_aov,
     control = control
   )
   
-  # Extract best results
-  best_model_params <- select_best(race_results, metric = "roc_auc")
-  best_model_metric <- show_best(race_results, metric = "roc_auc")
+  # Extract best results and Performance
+  performance_cv <- collect_metrics(race_results)
+  
+  best_model_params <- race_results %>% 
+    show_best(n = 1) %>%
+    select(-.metric, -.estimator, -mean, -n, -std_err, -.config)
   
   # Return
   return(list(
-    race_results = race_results,
+    performance_cv = performance_cv,
     best_model_params = best_model_params,
-    best_model_metric = best_model_metric
   ))
 }
 
-tune_race_aov(workflow = workfow,resamples = vfold_cv(data = data,v = 5))
+## Tune Race win or lose Function 
+tune_race_wl <- function(workflow,burn_in,resamples,size=20,params){
+  
+  # Define control race
+  control <- control_race(
+    randomize = TRUE,
+    burn_in = burn_in,
+    save_workflow = TRUE, 
+    save_pred = TRUE
+  )
+ 
+  # Set metric set
+  metric_aov <- metric_set(roc_auc)
+  
+  # Custom Grid based on the params
+  custom_grid <- grid_random(params, size = size) 
+  
+  # Perform tuning
+  race_results <- tune_race_win_loss(
+    object = workflow,
+    resamples = resamples,
+    grid = custom_grid,
+    metrics = metric_aov,
+    control = control
+  )
+  
+  # Extract best results and Performance
+  performance_cv <- collect_metrics(race_results)
+  
+  best_model_params <- race_results %>% 
+    show_best(n = 1) %>%
+    select(-.metric, -.estimator, -mean, -n, -std_err, -.config)
+  
+  # Return
+  return(list(
+    performance_cv = performance_cv,
+    best_model_params = best_model_params
+  ))
+}
+
+
+
+
+
+
+
+iris_recipe <- recipe(Species ~ ., data = iris) %>%
+  step_normalize(all_numeric_predictors())
+
+# 3. Model spec: random forest with default settings
+rf_spec <- rand_forest(mtry = tune(),trees = tune()) %>%
+  set_mode("classification") %>%
+  set_engine("ranger")
+
+# 4. Workflow
+rf_workflow <- workflow() %>%
+  add_recipe(iris_recipe) %>%
+  add_model(rf_spec)
+
+params <- parameters(list(
+  mtry(range = c(1,3)),
+  trees(range = c(100, 500))
+))
+
+resamples <- vfold_cv(data = iris, v = 4, strata = Species)
+a<-tune_race_wl(workflow = rf_workflow,burn_in = 3,resamples = resamples,size = 10,params =params)
+
+debug(tune_race_wl)
+
+#### MBO Function ####
+
+
+
+
+
+
+
+
 
 
 
