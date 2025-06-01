29e7246af810487cd6c7d3814a03c9571d2a8c35
mlatinov <latinovmetodi@gmail.com>
2025-06-01 23:29:17 +0300

    add local explanations functions

--- functions/explanatory_model_functions.R
+++ functions/explanatory_model_functions.R
@@ -103,23 +103,70 @@ global_explain <- function(data, model, label) {
 x <- global_explain(data = random_forest$preproc_data,model = random_forest$random_forest_fit,label = "Random Forest")
 
 ## Local Explanations 
-local_explain <- function(data,model,explainer,instance){
+local_explain <- function(data,model,instance,label){
+  
+  # Convert loan_status factor to numeric 
+  y_numeric <- as.numeric(as.character(data$loan_status))
+  
+  # Extract model from fitted workflow
+  model_only <- extract_fit_parsnip(model)
+  
+  # Predict function for parsnip model
+  predict_function <- function(model, newdata) {
+    predict(model, new_data = newdata, type = "prob")$.pred_1
+  }
+  
+  # Message
+  message("Creating Explainer")
+  
+  # Create explainer
+  explainer <- DALEX::explain(
+    model = model_only,
+    data = data %>% select(-loan_status),
+    y = y_numeric,
+    label = label,
+    type = "classification",
+    predict_function = predict_function,
+    verbose = FALSE
+  )
+  
+  # Message
+  message("Break Down Plots")
   
   # BD plots
+  break_down <- predict_parts(explainer = explainer,
+                              new_observation = instance,
+                              type = "break_down")
   
-  # iBD plots
+  break_down_plot <- plot(break_down)+
+    ggtitle("Break Down Plot")
+  
+  # Message
+  message("Shapley Additive Explanations")
   
   # Shapley Additive Explanations 
+  shap <- predict_parts(explainer = explainer,
+                        new_observation = instance,
+                        type = "shap")
   
-  # Local Interpretable Model-agnostic Explanations
+  shap_plot <- plot(shap,show_boxplots = FALSE)+
+    ggtitle("Shapley Additive Explanations")
   
-  # Ceteris-paribus (CP) profiles
-  
-  # Oscillations of CP profiles
-  
-  # Local-stability plot 
+  # Return
+  return(list(
+    break_down_plot = break_down_plot,
+    shap_plot = shap_plot,
+  ))
   
 }
+
+instance <- random_forest$preproc_data %>% slice(1)
+
+y <- local_explain(data = random_forest$preproc_data,
+                   model = random_forest$random_forest_fit,
+                   label = "Random Forest",
+                   instance = instance
+                   )
 
 
 
