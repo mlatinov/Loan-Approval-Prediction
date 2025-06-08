ec3fff5b8f8862111a96ad655659ef322fcf841e
mlatinov <latinovmetodi@gmail.com>
2025-06-09 00:10:54 +0300

    kaggle_clean function

--- functions/data_clean_fuction.R
+++ functions/data_clean_fuction.R
@@ -14,3 +14,18 @@ clean_eda <- function(data){
   
   return(data)
 }
+
+clean_eda_kaggle <- function(data){
+  
+  data <- data %>%
+    # Change data types
+    mutate(
+      loan_intent = str_to_lower(loan_intent),
+      person_home_ownership = str_to_lower(person_home_ownership)
+    )%>%
+    
+    # Convert all char into factors
+    mutate(across(where(is.character), as.factor))
+  
+  return(data)
+}
